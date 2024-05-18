d9cf8074d9bb172faaf30f7ce87ac124abf5c85d
fidria sari <22360009@stmik.jayakarta.ac.id>
2024-05-18 14:38:42 +0700

    reactjs

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <h1 style={{ fontSize: "24px", color: "white" }}>Home Movie List</h1>
+    <h2 style={{fontSize: "20px", color: "blue" }}>FIDRIA SARI</h2>
+    <h3 style={{ fontSize: "16px", color:"yellow" }}>Kelas Pemrograman Web</h3>
+  </div>
+);
+}
