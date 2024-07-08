2671b94847845b8292b95794f37d86d30da72a1a
fidria sari <22360009@stmik.jayakarta.ac.id>
2024-07-08 12:58:54 +0700

    update

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+"use client";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {images: {
+        domains: ['upload.wikimedia.org', 'lumiere-a.akamaihd.net', 'upload.wikimedia.org']
+}
+    
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+
+import React, { useState, useEffect } from 'react';
+import { Stack,Button,Card,CardText } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import MovieCard from "./MovieCard";
+import MovieForms from "./MovieForms";
+import Image from "next/image";
+
+
+
+export default function Home() {
+  return (
+    <div className="container">
+    <div className="row">
+      <div className="col-md-6">
+
+                    <h1 style={{ fontSize: "24px", color: "blue" }}>Home Movie List</h1>
+                    <h2 style={{ fontSize: "20px", color: "black" }}>FIDRIA SARI</h2>
+                    <h3 style={{ fontSize: "16px", color:"red" }}>Kelas Pemrograman Web</h3>
+
+            <Stack direction="horizontal" gap={2}>
+                <Button as="a" variant="primary">
+                 Button as link
+                    </Button>
+                    <Button as="a" variant="success">
+                      Button as link
+                    </Button>
+                  </Stack>
+
+                  </div>
+      </div>
+      
+      <div className="row">
+      <div className="col-md-8">
+        <br/>
+          <MovieForms />
+<br/>
+          </div>
+      </div>
+  <div className='bg-secondary' >
+        <MovieCard/>
+        </div>
+         </div>
+  );
+}
